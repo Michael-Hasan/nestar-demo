71385aa1c7ad5e84451b74f454eefc7a309857a2
Michael-Hasan <abdukhodirovhasan2002@gmail.com>
2026-02-01 19:38:26 +0900

    feat: Mobile version & Apollo client integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,9 @@ import { light } from "../scss/MaterialTheme";
 import { useState } from "react";
 import "../scss/app.scss";
 import "../scss/pc/main.scss";
+import "../scss/mobile/main.scss";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo/client";
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
@@ -12,9 +15,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
   // Socket.io, Redux, Mui ...
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,17 +9,46 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import TrendProperties from "@/libs/components/homepage/TrentProperties";
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "@/apollo/user/query";
 
 const Home: NextPage = () => {
-  return (
-    <Stack className={"home-page"}>
-      <TrendProperties />
-      <PopularProperties />
-      <Advertisement />
-      <TopProperties />
-      <TopAgents />
-    </Stack>
-  );
+  const device = useDeviceDetect();
+
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 5,
+        sort: "createdAt",
+        direction: "DESC",
+        search: {},
+      },
+    },
+  });
+
+  console.log("getPropertiesData =>", getPropertiesData);
+
+  if (device === "mobile") {
+    return <Stack>HOMEPAGE MOBILE</Stack>;
+  } else {
+    return (
+      <Stack className={"home-page"}>
+        <TrendProperties />
+        <PopularProperties />
+        <Advertisement />
+        <TopProperties />
+        <TopAgents />
+      </Stack>
+    );
+  }
 };
 
 export default withLayoutMain(Home);
